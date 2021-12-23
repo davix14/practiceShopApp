682b6d66abe3976b200767e075947681a18a0199
David Juarez <jdavidjuarez95@gmail.com>
2021-12-23 11:39:17 -0700

    STABLE - Added navcontroller to toolbar and added labels to all fragments

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,6 +2,11 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+//        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+
         Timber.plant(Timber.DebugTree())
     }
 }
